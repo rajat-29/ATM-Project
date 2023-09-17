e95d0323fa550b0804177d607d15464a5b290720
Rajat Gupta <rajat295gupta@gmail.com>
2023-09-17 11:14:47 +0530

    Account setup

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private int customerID;
+    private int pinNumber;
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public int getCustomerID() {
+        return this.customerID;
+    }
+
+    public int getPinNumber() {
+        return this.pinNumber;
+    }
+}
