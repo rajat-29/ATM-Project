67a9f53a24ccfd7ad8e9c3c6a599c83b8a8c1866
Rajat Gupta <rajat295gupta@gmail.com>
2023-09-17 19:16:30 +0530

    Added support for current account

--- src/OptionMenu.java
+++ src/OptionMenu.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class OptionMenu extends Account {
     Scanner menuInput = new Scanner(System.in);
     HashMap<Integer, Integer> data = new HashMap<>();
+    int selection;
 
+    // Login Functionality
     public void getLogin() throws IOException {
         int x = 1;
         do {
@@ -15,10 +17,10 @@ public class OptionMenu extends Account {
             data.put(123456, 789);
 
             System.out.println("Welcome to ATM :");
-            System.out.println("Enter your Customer ID : ");
+            System.out.print("Enter your Customer ID : ");
             setCustomerID(menuInput.nextInt());
 
-            System.out.println("Enter your PIN Number : ");
+            System.out.print("Enter your PIN Number : ");
             setPinNumber(menuInput.nextInt());
 
             } catch(Exception e) {
@@ -30,11 +32,64 @@ public class OptionMenu extends Account {
             int pinNumber = getPinNumber();
 
             if(data.containsKey(customerID) && data.get(customerID) == pinNumber) {
-
+                getAccountType();
             }
             else {
                 System.out.println("Wrong Customer ID or Pin Number, Please check again");
             }
         } while(x == 1);
+    }
+
+    // Display Account Type Menu with selection
+    public void getAccountType() {
+        System.out.println("Select the Account you want to access : ");
+        System.out.println("Type 1 - Current Account");
+        System.out.println("Type 2 - Saving Account");
+		System.out.println("Type 3 - Exit");
+		System.out.print("Choice: ");
+        selection = menuInput.nextInt();
+
+        switch(selection) {
+            case 1 : 
+                getCurrentAccount();
+                break;
+            default :
+                System.out.println("\n" + "Invalid Choice." + "\n");
+                getAccountType();
+        }
+    }
+
+    public void getCurrentAccount() {
+        System.out.println("Checking Account:  ");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Funds");
+        System.out.println("Type 3 - Deposit Funds");
+		System.out.println("Type 4 - Exit");
+		System.out.print("Choice: ");
+        selection = menuInput.nextInt();
+
+        switch(selection) {
+            case 1 :
+                System.out.println("Current Account Balance :" + getCurrentAccountBalance() + "\n");
+                break;
+
+            case 2 : 
+                withDrawFundsFromCurrent();
+			    getAccountType();
+                break;
+
+            case 3 : 
+                depositFundsInCurrent();
+                getAccountType();
+                break;
+
+            case 4 :
+                System.out.println("Thank You for using this ATM, bye.");
+                break;
+
+            default : 
+                System.out.println("\n" + "Invalid choice." + "\n");
+                getCurrentAccount();
+        }
     }
 }
